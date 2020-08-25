029d7ad3a89f862b0e59f7b13b0579004b40b0cc
jacktzw <1411840699@qq.com>
2020-08-25 22:45:50 +0800

    初始化

---
+++ day26_集合/src/com/qf/linkedlist/Node.java
@@ -0,0 +1,31 @@
+package com.qf.linkedlist;
+
+public class Node {
+    private Object previous;
+    private Object obj;
+    private Object next;
+
+    public Object getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Object previous) {
+        this.previous = previous;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+
+    public Object getNext() {
+        return next;
+    }
+
+    public void setNext(Object next) {
+        this.next = next;
+    }
+}
